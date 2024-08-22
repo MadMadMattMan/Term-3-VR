683d9f27ce215a70616baed9c84bf909248bab34
MadMadMattMan <92753107+MadMadMattMan@users.noreply.github.com>
2024-08-22 12:26:48 +1200

    0.13

--- Assets/Scripts/Whiteboard Marker.cs
+++ Assets/Scripts/Whiteboard Marker.cs
@@ -1,14 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using UnityEngine;
 
 public class WhiteboardMarker : MonoBehaviour
 {
     [SerializeField] Transform tip;
     [SerializeField] int size, interpolationStepPercent;
-
-    Renderer renderer;
+    [SerializeField] Color penColor;
     Color[] colors;
     float tipHeight;
     RaycastHit raycastResult;
@@ -20,40 +20,49 @@ public class WhiteboardMarker : MonoBehaviour
 
     void Start()
     {
-        //Sets up variables
-        renderer = GetComponent<Renderer>();
-        colors = Enumerable.Repeat(renderer.material.color, size*size).ToArray();
         tipHeight = tip.localScale.y;
+        colors = Enumerable.Repeat(penColor, size * size).ToArray();
     }
 
-    private void Update()
+    void Update()
     {
         Draw();
     }
 
     void Draw()
     {
+        Debug.DrawRay(tip.position, transform.forward, Color.red, 0.1f);
+
         //Find out if pen tip is touching the whiteboard
-        if (Physics.Raycast(tip.position, transform.up, out raycastResult, tipHeight))
+        if (Physics.Raycast(tip.position, transform.forward, out raycastResult, tipHeight))
         {
+            Debug.Log("ray hit");
             if (raycastResult.transform.CompareTag("Whiteboard Canvas"))
             {
+                Debug.Log("ray hit whiteboard");
+
                 //Sets the whiteboard script if is not already set
                 if (whiteboard == null)
                 {
                     whiteboard = raycastResult.transform.GetComponent<Whiteboard>();
                 }
 
+                Debug.Log(whiteboard);
+
                 //Gets a Vector2 position of where the pen tip is touching on the whiteboard
                 touchPoint = new Vector2(raycastResult.textureCoord.x, raycastResult.textureCoord.y);
 
                 //Converts this Vector2 position to a pixel coordinate location using the texture
+
+                //XY is problemssahdfkjsadhfkasldfasdflasdjgflsjdfjasdlfasdjfdsfkasdfkjsdhf
                 int x = (int)(touchPoint.x * whiteboard.textureSize.x - (size/2));
                 int y = (int)(touchPoint.x * whiteboard.textureSize.y - (size/2));
+                Debug.Log(x + ", " + y);
 
                 //if pen become out of bounds, stop the script
                 if (y < 0 || y > whiteboard.textureSize.y || x < 0 || x > whiteboard.textureSize.x)
                     return;
+
 
                 if (touchLastFrame)
                 {
@@ -70,8 +79,19 @@ public class WhiteboardMarker : MonoBehaviour
                         //Draw an inerpolated dot along the expected pathway
                         whiteboard.texture.SetPixels(lerpX, lerpY, size, size, colors);
                     }
+
+                    //Apply the changes
+                    whiteboard.texture.Apply();
                 }
+
+
+                lastTouchPoint = new Vector2(x, y);
+                touchLastFrame = true;
+                return;
             }
         }
+
+        whiteboard = null;
+        touchLastFrame = false;
     }
 }
