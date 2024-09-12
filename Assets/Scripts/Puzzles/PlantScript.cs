f32e8f0128a12b7d613e278ecd5e1697dbc917b9
MadMadMattMan <92753107+MadMadMattMan@users.noreply.github.com>
2024-09-12 12:26:56 +1200

    0.29

--- Assets/Scripts/Puzzles/PlantScript.cs
+++ Assets/Scripts/Puzzles/PlantScript.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.ParticleSystemJobs;
 
 public class PlantScript : MonoBehaviour
 {
-    [SerializeField] bool growing;
+    [SerializeField] bool growing = false;
     [SerializeField] float rate = 0.00001f;
     [SerializeField] Transform tf;
-    void Update()
+
+    void LateUpdate()
     {
         if (growing && tf.localScale.x < 0.0007f)
         {
@@ -15,13 +17,23 @@ public class PlantScript : MonoBehaviour
         }
     }
 
-    public void RecevingWaterChange(bool started)
+
+    private void OnTriggerEnter(Collider other)
     {
-       growing = started;
+        StartCoroutine(enableGrow());
     }
 
     Vector3 growVector()
     {
         return new Vector3(rate * Time.deltaTime, rate * Time.deltaTime, rate * Time.deltaTime);
     }
+
+    IEnumerator enableGrow()
+    {
+        Debug.Log("detected particle");
+        growing = true;
+        yield return new WaitForEndOfFrame();
+        growing = false;
+    }
+
 }
