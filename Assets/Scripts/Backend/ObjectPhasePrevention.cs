f211ce05c34572051f54b47bab5398bd5d89a8fc
MadMadMattMan <matthewcla3k@gmail.com>
2024-09-16 08:05:29 +1200

    0.22

--- Assets/Scripts/Backend/ObjectPhasePrevention.cs
+++ Assets/Scripts/Backend/ObjectPhasePrevention.cs
@@ -4,6 +4,27 @@ using UnityEngine;
 
 public class ObjectPhasePrevention : MonoBehaviour
 {
-    
-    
+    Vector3 startPos;
+    Quaternion startRot;
+
+    float minY = -0.1f;
+
+    private void Start()
+    {
+        startPos = transform.position;
+        startRot = transform.rotation;
+    }
+
+    private void Update()
+    {
+        if (transform.position.y < minY)
+        {
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+            GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+
+            transform.position = startPos;
+            transform.rotation = startRot;
+        }
+    }
+
 }
---
+++ Assets/Scripts/Misc/HammerHit.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HammerHit : MonoBehaviour
+{
+    ParticleSystem ParticleSystem;
+    AudioSource AudioSource;
+
+    private void Start()
+    {
+        ParticleSystem = GetComponentInChildren<ParticleSystem>();
+        AudioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Get Color
+        ParticleSystem.startColor = Color.black;
+        ParticleSystem.Play();
+        AudioSource.Play();
+    }
+}
