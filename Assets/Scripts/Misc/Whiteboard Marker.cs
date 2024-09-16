80002f69eebfead802dadfd913583df31c40a4d0
MadMadMattMan <matthewcla3k@gmail.com>
2024-09-17 11:52:40 +1200

    0.25

--- Assets/Scripts/Misc/Whiteboard Marker.cs
+++ Assets/Scripts/Misc/Whiteboard Marker.cs
@@ -6,11 +6,11 @@ using UnityEngine;
 
 public class WhiteboardMarker : MonoBehaviour
 {
-    [SerializeField] Transform[] tip;
+    [SerializeField] Transform tip;
     [SerializeField] int size;
     [SerializeField] float interpolationStepPercent;
     [SerializeField] Color penColor;
-    [SerializeField] Whiteboard linkedWhiteboard;
+    [SerializeField] Rigidbody rb;
 
     Color[] colors;
     [SerializeField] float tipHeight;
@@ -20,27 +20,22 @@ public class WhiteboardMarker : MonoBehaviour
 
     bool touchLastFrame;
 
-    Rigidbody rb;
-
 
     void Start()
     {
         whiteboard = GameObject.FindWithTag("Whiteboard Canvas").GetComponent<Whiteboard>();
 
-        rb = GetComponent<Rigidbody>();
-        tipHeight = tip[0].localScale.y;
+        if (rb == null)
+            rb = GetComponent<Rigidbody>();
+        
+        tipHeight = tip.localScale.y;
+
         if (gameObject.name == "duster")
-        {
             penColor = whiteboard.texture.GetPixel(0, 0);
-            colors = Enumerable.Repeat(penColor, size * size * 3).ToArray();
-        }
-        else
-            colors = Enumerable.Repeat(penColor, size * size).ToArray();
 
+        colors = Enumerable.Repeat(penColor, size * size).ToArray();
 
-
-
-
+        whiteboard = null;
     }
 
     void Update()
@@ -52,95 +47,68 @@ public class WhiteboardMarker : MonoBehaviour
 
     void Draw(int loop)
     {
-        for (int i = 0; i < tip.Length; i++)
+        //Debug to see ray
+        Debug.DrawRay(tip.position, tip.transform.forward / 0.9f, Color.red, 0.01f);
+
+        //Find out if pen tip is touching the whiteboard
+        if (Physics.Raycast(tip.position, tip.transform.forward, out raycastResult, tipHeight))
         {
-            if (loop != 0)
-                i++;
-
-            Debug.DrawRay(tip[i].position, tip[i].transform.forward, Color.red, 0.01f);
-
-            //Find out if pen tip is touching the whiteboard
-            if (Physics.Raycast(tip[i].position, tip[i].transform.forward, out raycastResult, tipHeight))
+            Debug.Log("ray hit for " + gameObject.name + " at " + raycastResult.collider.gameObject.name);
+            if (raycastResult.transform.CompareTag("Whiteboard Canvas"))
             {
-                Debug.Log("ray hit for " + gameObject.name + " at " + raycastResult.collider.gameObject.name);
-                if (raycastResult.transform.CompareTag("Whiteboard Canvas"))
-                {
-                    ///Debug.Log("ray hit whiteboard");
+                ///Debug.Log("ray hit whiteboard");
+                //Sets the whiteboard script if is not already set
+                if (whiteboard == null)
+                    whiteboard = raycastResult.transform.GetComponent<Whiteboard>();
 
-                    //Sets the whiteboard script if is not already set
-                    if (whiteboard == null)
-                        whiteboard = raycastResult.transform.GetComponent<Whiteboard>();
+                //Gets a Vector2 position of where the pen tip is touching on the whiteboard
+                touchPoint = new Vector2(raycastResult.textureCoord.x, raycastResult.textureCoord.y);
 
-                    //Gets a Vector2 position of where the pen tip is touching on the whiteboard
-                    touchPoint = new Vector2(raycastResult.textureCoord.x, raycastResult.textureCoord.y);
+                //Converts this Vector2 UV position to a pixel coordinate location using the texture
+                ///Debug.Log("Touch points " + touchPoint.x + ", " + touchPoint.y + "; texture size " + whiteboard.textureSize.x + ", " + whiteboard.textureSize.y + "; pen size" + size);
+                var x = (int)(touchPoint.x * whiteboard.textureSize.x - (size / 2));
+                var y = (int)(touchPoint.y * whiteboard.textureSize.y - (size / 2));
+                ///Debug.Log(x + ", " + y);
 
-
-
-
-                    //Converts this Vector2 UV position to a pixel coordinate location using the texture
-
-                    ///Debug.Log("Touch points " + touchPoint.x + ", " + touchPoint.y + "; texture size " + whiteboard.textureSize.x + ", " + whiteboard.textureSize.y + "; pen size" + size);
-                    var x = (int)(touchPoint.x * whiteboard.textureSize.x - (size / 2));
-                    var y = (int)(touchPoint.y * whiteboard.textureSize.y - (size / 2));
-                    ///Debug.Log(x + ", " + y);
-
-                    //if pen leaves the bounds of the whiteboard, stop the script
-                    if (y < 0 || y > whiteboard.textureSize.y || x < 0 || x > whiteboard.textureSize.x)
-                    {
-                        if (loop != tip.Length - 1)
-                        {
-                            //as i = loop + 1
-                            Draw(i);
-                        }
-                        return;
-                    }
-
-
-
-                    //if the pen was touching the whiteboard last frame
-                    if (touchLastFrame)
-                    {
-                        //Draw exactly where the pen tip is touching
-                        if (gameObject.name == "duster")
-                            whiteboard.texture.SetPixels(x, y, size * 3, size, colors);
-                        else
-                            whiteboard.texture.SetPixels(x, y, size, size, colors);
-                        Debug.Log(gameObject.name + " drew on " + whiteboard.gameObject.name);
-
-                        //Interpolates the position between frames so line stays solid and not just dots
-                        for (float j = 0.01f; j < 1; j += interpolationStepPercent / 100)
-                        {
-                            //Get the interpolated steps
-                            int lerpX = (int)Mathf.Lerp(lastTouchPoint.x, x, j);
-                            int lerpY = (int)Mathf.Lerp(lastTouchPoint.y, y, j);
-
-                            //Draw an inerpolated dot along the expected pathway
-                            whiteboard.texture.SetPixels(lerpX, lerpY, size, size, colors);
-                        }
-
-                        //Apply the changes to the texture
-                        whiteboard.texture.Apply();
-                        whiteboard.drawn = true;
-                    }
-
-                    //Setup for next frame
-                    lastTouchPoint = new Vector2(x, y);
-                    touchLastFrame = true;
-                    if (loop != tip.Length - 1)
-                    {
-                        //as i = loop + 1
-                        Draw(i);
-                    }
+                //if pen leaves the bounds of the whiteboard, stop the script
+                if (y < 0 || y > whiteboard.textureSize.y || x < 0 || x > whiteboard.textureSize.x)
                     return;
 
+
+
+                //if the pen was touching the whiteboard last frame
+                if (touchLastFrame)
+                {
+                    //Draw exactly where the pen tip is touching
+                    whiteboard.texture.SetPixels(x, y, size, size, colors);
+                    Debug.Log(gameObject.name + " drew on " + whiteboard.gameObject.name);
+
+                    //Interpolates the position between frames so line stays solid and not just dots
+                    for (float j = 0.01f; j < 1; j += interpolationStepPercent / 100)
+                    {
+                        //Get the interpolated steps
+                        int lerpX = (int)Mathf.Lerp(lastTouchPoint.x, x, j);
+                        int lerpY = (int)Mathf.Lerp(lastTouchPoint.y, y, j);
+
+                        //Draw an inerpolated dot along the expected pathway
+                        whiteboard.texture.SetPixels(lerpX, lerpY, size, size, colors);
+                    }
+
+                    //Apply the changes to the texture
+                    whiteboard.texture.Apply();
+                    whiteboard.drawn = true;
                 }
+
+                //Setup for next frame
+                lastTouchPoint = new Vector2(x, y);
+                touchLastFrame = true;
+                return;
+
             }
         }
-
 
         //If pen is released from the whiteboard, reset
         whiteboard = null;
         touchLastFrame = false;
-
     }
 }
