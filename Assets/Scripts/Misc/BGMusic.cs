99653f1ff5c8e140ebb8ea0baf02bca2ae65edbf
MadMadMattMan <matthewcla3k@gmail.com>
2024-09-26 23:45:56 +1200

    0.30

---
+++ Assets/Scripts/Misc/BGMusic.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMusic : MonoBehaviour
+{
+    [SerializeField] AudioClip[] musicClips;
+    [SerializeField] AudioSource player;
+    int clipNum = 0;
+    float time = 0f, fadeTime = 5f;
+    bool fadeMusic = false;
+    float maxVolume = 0.015f;
+
+    private void Update()
+    {
+        if (time > 1)
+        {
+            if (!player.isPlaying)
+            {
+                player.volume = 0;
+                player.PlayOneShot(musicClips[clipNum]);
+                fadeMusic = true;
+                clipNum++;
+            }
+        }
+
+        if (fadeMusic == true)
+        {
+            player.volume += maxVolume / fadeTime * Time.deltaTime;
+            if (player.volume >= maxVolume)
+                fadeMusic = false;
+        }
+
+        time += Time.deltaTime;
+    }
+}
