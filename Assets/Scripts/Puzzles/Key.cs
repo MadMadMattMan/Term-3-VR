366261b067be7a00d0bd318edb183e12de582c68
MadMadMattMan <92753107+MadMadMattMan@users.noreply.github.com>
2024-10-24 12:46:54 +1300

    0.45

--- Assets/Scripts/Puzzles/Key.cs
+++ Assets/Scripts/Puzzles/Key.cs
@@ -7,8 +7,9 @@ public class Key : MonoBehaviour
 {
     [SerializeField] GameObject lockObj;
     [SerializeField] Transform snapPos;
+    [SerializeField] GrabFreeTransformer lockedConstraint;
 
-    ConfigurableJoint joint;
+    Grabbable handGrab;
     Collider thisCollider;
     Rigidbody rb;
     bool inLock = false;
@@ -17,7 +18,7 @@ public class Key : MonoBehaviour
     {
         //Setup
         thisCollider = GetComponentInChildren<MeshCollider>();
-        joint = GetComponent<ConfigurableJoint>();
+        handGrab = GetComponent<Grabbable>();
         rb = GetComponent<Rigidbody>();
     }
 
@@ -25,23 +26,32 @@ public class Key : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Lock"))      
         {
-            ///Debug.Log("Key hit lock");
-
-            //Disables collisions for key and prevents free movement
-            thisCollider.isTrigger = true;
-            rb.isKinematic = true;
-
-            //Snaps key into lock
-            transform.parent = snapPos;
-            transform.localPosition = Vector3.zero;
-            transform.rotation = Quaternion.Euler(new Vector3(270, 180, 270));
-
-            //Makes the key fully attach to the lock, prevent key from respawing if lock faces into ground
-            Destroy(GetComponent<ObjectPhasePrevention>());
-
-            //Bool update
-            inLock = true;
+            LockFunction();
         }
+    }
+
+    void LockFunction()
+    {
+        ///Debug.Log("Key hit lock");
+
+        //Disables collisions for key and prevents free movement
+        thisCollider.isTrigger = true;
+        rb.isKinematic = true;
+
+        //Snaps key into lock
+        transform.parent = snapPos;
+        transform.localPosition = Vector3.zero;
+        transform.rotation = Quaternion.Euler(new Vector3(270, 180, 270));
+
+        //Makes the key fully attach to the lock, prevent key from respawing if lock faces into ground
+        Destroy(GetComponent<ObjectPhasePrevention>());
+
+        //Cause constained grab
+        handGrab.InjectOptionalOneGrabTransformer(lockedConstraint);
+        handGrab.InjectOptionalTwoGrabTransformer(lockedConstraint);
+
+        //Bool update
+        inLock = true;
     }
 
     private void Update()
@@ -49,12 +59,24 @@ public class Key : MonoBehaviour
         //If the key is in the lock, check for the rotation of the key and if the key has been turned
         if (inLock)
         {
+            rb.isKinematic = true;
+            transform.localPosition = Vector3.zero;
+
             Debug.Log(transform.localEulerAngles);
 
             //If in the lock and turned, unlock the lock
             if (transform.localRotation.eulerAngles.y > 295f)
             {
                 lockObj.GetComponentInParent<LockScript>().Unlock();
+            }
+        }
+        if (rb.isKinematic && !inLock)
+        {
+            float dist = Vector3.Distance(transform.position, lockObj.transform.position);
+
+            if (dist < 0.05f)
+            {
+                LockFunction();
             }
         }
     }
